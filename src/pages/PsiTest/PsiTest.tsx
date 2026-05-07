a29665f320575b268d3cd5dcba8b2c5e6f2ac978
wgokef <verlevskaasofia@gmail.com>
2026-05-07 16:24:04 +0300

    добавила функцию для счета плюсов в пси тест

--- src/pages/PsiTest/PsiTest.tsx
+++ src/pages/PsiTest/PsiTest.tsx
@@ -1,21 +1,27 @@
-import { questions } from "@/utils/helpers/PsiTest.help"
-import { useState } from "react"
-
+import { questions } from "@/utils/helpers/PsiTest.help";
+import { useState } from "react";
 
 const PsiTest = () => {
-    const [plus, setPlus] = useState(0);
-    const [minus, setMinus] = useState(0);
-    return <div>
-        {questions.map((item, index) => (
-            <p>
-                {index+1} {item}
-            </p>
-        ))}
-        <button>да</button>
-        <button> нет</button>
-    plus:{plus}
-    minus:{minus}
-    </div>
-}
+  const [plus, setPlus] = useState(0);
+  const [minus, setMinus] = useState(0);
 
-export default PsiTest
+  const addPlus = () => {
+    setPlus(plus + 1);
+  };
+
+  return (
+    <div>
+      {questions.map((item, index) => (
+        <p>
+          {index + 1} {item}
+        </p>
+      ))}
+      <button onClick={addPlus}>да</button>
+      <button> нет</button>
+      plus:{plus}
+      minus:{minus}
+    </div>
+  );
+};
+
+export default PsiTest;
