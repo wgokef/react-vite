35bc103020e4fa34b0e0ecbf15dd34e994b1bff8
wgokef <verlevskaasofia@gmail.com>
2026-05-07 16:09:11 +0300

    вывела часть вопросов по пси тесту

--- src/pages/PsiTest/PsiTest.tsx
+++ src/pages/PsiTest/PsiTest.tsx
@@ -1,8 +1,14 @@
-
+import { questions } from "@/utils/helpers/PsiTest.help"
 
 
 const PsiTest = () => {
+    
     return <div>
+        {questions.map((item, index) => (
+            <p>
+                {index+1} {item}
+            </p>
+        ))}
         <button>да</button>
         <button> нет</button>
     </div>
